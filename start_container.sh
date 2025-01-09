40fdc394691be08d973839267b28b6d9a14c1f33
Rashika Sahu <93260606+rashikasahu35@users.noreply.github.com>
2025-01-09 14:25:19 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
+
 # Pull the Docker image from Docker Hub
-do
-docker pull rashikasahu/python-app:latest
+sudo docker pull rashikasahu/python-app:latest
 
 # Run the Docker image as a container
-docker run -t -p 5000:5000 rashikasahu/python-app:latest
+sudo docker run -t -p 5000:5000 rashikasahu/python-app:latest
